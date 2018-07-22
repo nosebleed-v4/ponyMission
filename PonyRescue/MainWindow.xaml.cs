d521afcda30814a5cc38682c1e9bdb63c02252d3
Bart Gebski (BAGE) <bage@oticon.com>
2018-07-22 15:52:04 +0200

    Add project files.

---
+++ PonyRescue/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace PonyRescue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string responseBody = string.Empty;
+            string url = @"https://ponychallenge.trustpilot.com/pony-challenge/maze/7ae65af1-f797-441f-9df2-893821da0e04";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                responseBody = reader.ReadToEnd();
+            }
+
+            MazeState mazeState = JsonConvert.DeserializeObject<MazeState>(responseBody);
+            //Console.WriteLine(responseBody);
+        }
+    }
+}
---
+++ PonyRescue/gameSate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PonyRescue
+{
+    public class GameState
+    {
+        private GameState() { }
+
+        [JsonProperty("state")]
+        public string gameState { get; set; }
+
+        [JsonProperty("state-result")]
+        public string gameResult { get; set; }
+    }
+
+    public class MazeState
+    {
+        private MazeState() { }
+
+        [JsonProperty("pony")]
+        private List<int> ponyLocationRaw;
+
+        public int PonyLocation => ponyLocationRaw[0];
+
+        [JsonProperty("domokun")]
+        private List<int> domokunLocationRaw { get; set; }
+        public int DomokunLocation => domokunLocationRaw[0];
+
+        [JsonProperty("end-point")]
+        private List<int> exitLocationRaw { get; set; }
+        public int ExitLocation => exitLocationRaw[0];
+
+        [JsonProperty("size")]
+        private List<int> sizeRaw { get; set; }
+        public int width => sizeRaw[0];
+        public int height => sizeRaw[1];
+
+        [JsonProperty("difficulty")]
+        public int difficulty { get; set; }
+
+        [JsonProperty("data")]
+        public List<List<string>> data { get; set; }
+
+        [JsonProperty("maze_id")]
+        public string maze_id { get; set; }
+
+        [JsonProperty("game-state")]
+        public GameState gameState { get; set; }
+    }
+}
